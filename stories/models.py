b83e10888b25323c4c616a1e4f168b07bf761229
Christopher Roach <vthakr@gmail.com>
2012-11-21 12:09:06 -0800

    Added default value to points and __unicode__ method

--- stories/models.py
+++ stories/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Story(models.Model):
     title = models.CharField(max_length=200)
     url = models.URLField()
-    points = models.IntegerField()
+    points = models.IntegerField(default=0)
     moderator = models.ForeignKey(User)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -14,3 +14,6 @@ class Story(models.Model):
     @property
     def domain(self):
         return urlparse(self.url).netloc
+
+    def __unicode__(self):
+        return self.title
