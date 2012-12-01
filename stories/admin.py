1ac57c1f62637e5ba001e5014d005b7a7557f677
Christopher Roach <vthakr@gmail.com>
2012-12-01 01:08:18 -0800

    Added fieldsets to the edit form

--- stories/admin.py
+++ stories/admin.py
@@ -9,5 +9,20 @@ class StoryAdmin(admin.ModelAdmin):
     list_filter = ('created_at', 'updated_at')
     search_fields = ('title', 'moderator__username', 'moderator__first_name', 'moderator__last_name')
 
+    fieldsets = [
+        ('Story', {
+            'fields': ('title', 'url', 'points')
+        }),
+        ('Moderator', {
+            'classes': ('collapse',),
+            'fields': ('moderator',)
+        }),
+        ('Change History', {
+            'classes': ('collapse',),
+            'fields': ('created_at', 'updated_at')
+        })
+    ]
+    readonly_fields = ('created_at', 'updated_at')
+
 
 admin.site.register(Story, StoryAdmin)
