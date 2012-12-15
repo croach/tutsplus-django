b7902268780e199c9142758030f1e5d4c8317155
Christopher Roach <vthakr@gmail.com>
2012-12-15 12:58:25 -0800

    Added the story form URL pattern

--- hackernews/urls.py
+++ hackernews/urls.py
@@ -5,6 +5,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', include('stories.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('stories.urls')),
 )
--- stories/urls.py
+++ stories/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'stories.views.index'),
+    url(r'^story/$', 'stories.views.story'),
 )
 
