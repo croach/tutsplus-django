6d55a67503f8e449e9e202a3ff1a5ebc02728e5b
Christopher Roach <vthakr@gmail.com>
2012-12-15 12:57:25 -0800

    Added the story form view and template

--- stories/views.py
+++ stories/views.py
@@ -1,9 +1,11 @@
 import datetime
 
-from django.shortcuts import render_to_response
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.utils.timezone import utc
 
 from stories.models import Story
+from stories.forms import StoryForm
 
 def score(story, gravity=1.8, timebase=120):
     points = (story.points - 1)**0.8
@@ -19,4 +21,14 @@ def top_stories(top=180, consider=1000):
 
 def index(request):
     stories = top_stories(top=30)
-    return render_to_response('stories/index.html', {'stories': stories})
+    return render(request, 'stories/index.html', {'stories': stories})
+
+def story(request):
+    if request.method == 'POST':
+        form = StoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = StoryForm()
+    return render(request, 'stories/story.html', {'form': form})
