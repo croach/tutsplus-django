7691055185f9e05caef5c02362a566cfc9f2b27f
Christopher Roach <vthakr@gmail.com>
2012-12-15 12:57:05 -0800

    Added the story form

---
+++ stories/forms.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm
+
+from stories.models import Story
+
+
+class StoryForm(ModelForm):
+    class Meta:
+        model = Story
+        exclude = ('points', 'moderator',)
