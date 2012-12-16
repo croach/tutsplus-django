7c49b323cf6e7d95b611aa37517c3d776058f65f
Christopher Roach <vthakr@gmail.com>
2012-12-15 21:38:49 -0800

    Added the voters relationship to the Story model

--- stories/models.py
+++ stories/models.py
@@ -7,7 +7,8 @@ class Story(models.Model):
     title = models.CharField(max_length=200)
     url = models.URLField()
     points = models.IntegerField(default=1)
-    moderator = models.ForeignKey(User)
+    moderator = models.ForeignKey(User, related_name='moderated_stories')
+    voters = models.ManyToManyField(User, related_name='liked_stories')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
