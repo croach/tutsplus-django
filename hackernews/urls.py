06de62ab248671f50f18276af46e3a555ddafe75
Christopher Roach <vthakr@gmail.com>
2012-12-18 16:40:51 -0800

    Added names for each of the URL patterns

--- hackernews/urls.py
+++ hackernews/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'auth/login.html'}),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'auth/login.html'}, name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
     url(r'', include('stories.urls')),
 )
--- stories/urls.py
+++ stories/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^$', 'stories.views.index'),
-    url(r'^story/$', 'stories.views.story'),
-    url(r'^vote/$', 'stories.views.vote'),
+    url(r'^$', 'stories.views.index', name='index'),
+    url(r'^story/$', 'stories.views.story', name='story'),
+    url(r'^vote/$', 'stories.views.vote', name='vote'),
 )
 
